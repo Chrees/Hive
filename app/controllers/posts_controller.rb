840a807c56a520223a078fd80ed7d4cd58638c54
Chrees <christian.schuerch@hotmail.com>
2015-07-31 14:55:42 +0200

    Add Post model and basic views

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,49 @@
+class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post, notice: "Successfully created new post"
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post, notice: "Post was successfully updated!"
+    else
+      render 'edit'
+    end
+  end
+
+  def edit
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
+  end
+
+  def index
+    @posts = Post.all.order("created_at DESC")
+  end
+
+  def show
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:source, :description)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+end
