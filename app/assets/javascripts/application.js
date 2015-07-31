79d1f54ebbe3a5dde3ff36e9130cff027080d9cd
Chrees <christian.schuerch@hotmail.com>
2015-07-31 15:10:59 +0200

    Add structure and styles

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,99 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
-//= require turbolinks
+
+//= require mousetrap
+//= require bootstrap-sprockets
+
+// require turbolinks
 //= require_tree .
+//= require_self
+
+$(window).load(function() {
+   show();
+});
+
+function show() {
+    $('.loader').delay(1000).hide();
+    $('body').fadeIn();
+};
+
+setTimeout(show, 3000);
+
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('.navbar-default').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+$('html').keydown(function (e) {
+	if (e.keyCode == 38 || e.keyCode == 67) {
+		$('.navbar-default').toggleClass('nav-down').toggleClass('nav-up');
+	}
+});
+
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st < lastScrollTop){
+        // Scroll Down
+        $('.navbar-default').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('.navbar-default').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+    
+    lastScrollTop = st;
+}
+
+
+
+$('a[href*=#]:not([href=#])').keydown(function(e) {
+    if (e.keyCode == 87 || e.keyCode == 83) {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+        }
+    }
+});
+
+
+// For Flash-notices
+$(document).ready(function(){
+    setTimeout(function(){
+        $('#notice-wrapper').fadeOut("slow", function(){
+            $(this).remove();
+        })
+    }, 3141);
+});
+
+
+
