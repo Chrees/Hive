4de7a25e982c4ccbee4455de1f2bf50397ddd206
Chrees <christian.schuerch@hotmail.com>
2015-07-31 14:58:56 +0200

    Add Devise User and Post/User association

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,11 +2,12 @@ class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy]
 
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.build(post_params)
+    
     if @post.save
       redirect_to @post, notice: "Successfully created new post"
     else
