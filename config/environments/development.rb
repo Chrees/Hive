b4a7e9edebc4025dde11c8a374b04550e9e709f6
Chrees <christian.schuerch@hotmail.com>
2015-08-04 15:46:46 +0200

    Refix development.rb and production.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,13 +43,13 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   #Needed for Paperclip
-  #Paperclip.options[:command_path] = "/Program Files/ImageMagick-6.9.1-Q16"
-  config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['AWS_BUCKET'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
-  }
+  Paperclip.options[:command_path] = "/Program Files/ImageMagick-6.9.1-Q16"
+  #config.paperclip_defaults = {
+  #  :storage => :s3,
+  #  :s3_credentials => {
+  #    :bucket => ENV['AWS_BUCKET'],
+  #    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+  #    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  #  }
+  #}
 end
