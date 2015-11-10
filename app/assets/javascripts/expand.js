870482d02e545ed0cdb9e56c8fa6995f264a42db
Chrees <christian.schuerch@hotmail.com>
2015-11-10 11:19:36 +0100

    Expand tables by default

--- app/assets/javascripts/expand.js
+++ app/assets/javascripts/expand.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
-    $("table tbody").removeClass("odd");
+    $("table tbody").addClass("odd");
     $("table tbody:not(.odd)").hide();
     //$("table tr:first-child").show();
     
     $("table thead").click(function(){
-        $(this).next("tbody").toggle(600);
+        $(this).next("tbody").toggle(300);
     });
     //$("table").jExpand();
 });	
